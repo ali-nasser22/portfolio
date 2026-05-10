37129c79a27d7cb7c6737abc7b961aba7a40e89f
Ali Nasser <alinasser.t22@gmail.com>
2026-05-10 14:11:26 +0300

    add: Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import "../styles/globals.css";
+import {Analytics} from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "AliOS 95 — Ali Nasser Portfolio",
@@ -10,8 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-
-        <body>{children}</body>
+        <body>
+        {children}
+        <Analytics/>
+        </body>
         </html>
     );
 }
